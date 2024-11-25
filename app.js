8cdb1edac556d008163cb266ebf7b19457084632
BibinPJohnson <90170319+BibinPJohnson@users.noreply.github.com>
2024-11-25 11:45:54 +0530

    Update app.js

--- app.js
+++ app.js
@@ -11,6 +11,11 @@ app.use(express.json());
 // Use routes from the `routes` folder
 app.use('/api', routes);
 
+
+app.get('/testserver', (req, res) => {
+  res.json({ summary: { status: "SUCCESS", reason: "SERVER_RUNNING" } });
+});
+
 // Start the server
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}/`);
